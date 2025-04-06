f3e5972e8301e95ebcbcd4cc18a4ab2ef60848b3
kebechet-200 <lashasuliash@gmail.com>
2025-04-06 12:28:00 +0400

    add clients

---
+++ Client2/RealTimeChatApplicationClient2/Program.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Net.Client;
+using Grpc.Core;
+using System;
+using ChatApp.Protos;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("https://localhost:7265");
+        var client = new Chat.ChatClient(channel);
+
+        using var chat = client.ChatStream();
+
+        // Start reading messages from the server
+        var responseTask = Task.Run(async () =>
+        {
+            await foreach (var message in chat.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine($"{message.User}: {message.Message}");
+            }
+        });
+
+        // Sending messages
+        Console.WriteLine("Enter your name:");
+        var user = Console.ReadLine();
+
+        Console.WriteLine("You can now start chatting:");
+        string? line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            await chat.RequestStream.WriteAsync(new ChatMessage
+            {
+                User = user,
+                Message = line
+            });
+        }
+
+        await chat.RequestStream.CompleteAsync();
+        await responseTask;
+    }
+}
